7ada0dc35f9c68842eab8a1145fdef62c7608c91
FrancescoSalgari <salgari.francesco03@gmail.com>
2024-06-17 11:02:06 +0200

    webfontloader

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,6 @@
 //import HelloWorld from './components/HelloWorld.vue'
 </script>
 
-
 <template>
   <header>
     <!-- <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
@@ -18,7 +17,7 @@
      </div> -->
 
     <!--  prova -->
-      <!-- ciao -->
+    <!-- ciao -->
   </header>
   <RouterView />
 </template>
